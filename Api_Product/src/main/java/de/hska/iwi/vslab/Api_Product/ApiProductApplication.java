633f1eb82db34dddbc27ceb86276fc89a3297479
and1kl <40032530+and1kl@users.noreply.github.com>
2019-11-12 18:33:06 +0100

    Spring Boot Project Initial

---
+++ Api_Product/src/main/java/de/hska/iwi/vslab/Api_Product/ApiProductApplication.java
@@ -0,0 +1,13 @@
+package de.hska.iwi.vslab.Api_Product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiProductApplication.class, args);
+	}
+
+}
